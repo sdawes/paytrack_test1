39d6266efb3e5a2d816192ff2dac68570ac1da22
Stephen Dawes <stephenrdawes@gmail.com>
2025-04-07 19:37:51 +0100

    Initial Commit

---
+++ paytrack_test1/paytrack_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  paytrack_test1App.swift
+//  paytrack_test1
+//
+//  Created by Stephen Dawes on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct paytrack_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
